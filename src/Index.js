fd88d616a131ad3ae88983f994f7301b79f31df1
Gabriel Pinheiro <g.felippe5965@gmail.com>
2022-02-23 13:39:00 -0300

    doc: creating README.md on master

---
+++ src/Index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <>Escolha a branch do exercício: digitando "git checkout nome-da-branch"</>,
+  document.getElementById("root")
+);
